9ebfa034fe4dda29a8fc0bd1dd025bff5b35b0cf
crab <crabtrucker@gmail.com>
2021-08-06 20:18:36 -0400

    test

--- sqlHelpers.rb
+++ sqlHelpers.rb
@@ -40,8 +40,8 @@ class SQLMethods
 
 
         pgresult.each do |row|
-            Logger.log("The row value is #{row} and the 1 value is #{row[1]}", 0)
-            returnedIDs += row[1].to_s + ", "
+            Logger.log("The row value is #{row} and the 1 value is #{row['id']}", 0)
+            returnedIDs += row['id'].to_s + ", "
         end
 
         Logger.log("the returned ids are #{returnedIDs}", 0)
